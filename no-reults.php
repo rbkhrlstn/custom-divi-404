4b08f86c299136a4d7192342abf193bf0fd3e378
Rebekah R <me@rbkhrlstn.com>
2019-08-21 16:43:18 -0400

    Create no-reults.php

---
+++ no-reults.php
@@ -0,0 +1,5 @@
+<div class="entry">
+<!--If no results are found-->
+ <?php $id = THE ID OF YOUR DIVI LIBRARY LAYOUT GOES HERE; $p = get_page($id); echo apply_filters('the_content', $p->post_content); ?>
+</div>
+<!--End if no results are found-->
